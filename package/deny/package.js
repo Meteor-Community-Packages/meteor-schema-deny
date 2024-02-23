17e26605095e2ef8a1a2e9de3ed9d40e15d8d11b
Jan Dvorak <storytellercz@proton.me>
2024-02-23 12:26:53 +0100

    Update testing app & Published aldeed:schema-deny@4.0.0

--- package/deny/package.js
+++ package/deny/package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: "aldeed:schema-deny",
   summary: "Deny inserting or updating certain properties through schema options",
-  version: "4.0.0-beta.1",
+  version: "4.0.0",
   documentation: '../../README.md',
   git: "https://github.com/aldeed/meteor-schema-deny.git"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom(['2.8.1', '3.0-beta.0']);
+  api.versionsFrom(['2.8.1', '3.0-beta.4']);
   api.use('aldeed:collection2@4.0.0');
   api.use('aldeed:simple-schema@1.13.1');
   api.use('ecmascript');
